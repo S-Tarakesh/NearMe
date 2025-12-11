c0c370496a3df71df3cee221138229490862f88f
acer@TMP215-75-G2 <tarakesh.sj@gmail.com>
2025-12-11 14:15:02 +0530

    first commit

--- Tarakesh/Nearme/admin.py
+++ Tarakesh/Nearme/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+class NearmeAdmin(admin.AdminSite):
+    site_header = "Nearme Administration"
+    site_title = "Nearme Admin Portal"
+    index_title = "Welcome to Nearme Admin Portal"
+admin_site = NearmeAdmin(name='nearme_admin')
+  # Register your models here
